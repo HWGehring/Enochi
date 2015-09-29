6043ff9ebdb2b0c1385a7a12c851feaf73bb8449
hwgehring <hwgehring@gmail.com>
2015-09-28 22:39:54 -0400

    Refactored unit test

--- tests/lexing_test.py
+++ tests/lexing_test.py
@@ -1,11 +1,9 @@
 import unittest
-from enochi.lexer import TokenType, Token, lex
+from enochi.lexer import Lexer
 
 
 class LexingText(unittest.TestCase):
 
     def test_lex(self):
-        a = list(lex('1'))[0]
-        self.assertEqual(a.type, TokenType.integer)
-        self.assertEqual(a.value, 1)
-        self.assertEqual(type(a), Token)
+        l = Lexer('1')
+        print(l)
